ffb190994683683c0b4bdb0c28c6fcc0881424d5
AnemoneTK <Copp.teenkung@gmail.com>
2024-02-18 12:07:38 +0700

    [IMPORT]-import layout page from old project

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,24 +1,50 @@
-import { Route, Routes} from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // Route,
+  // Link,
+} from "react-router-dom";
+import "./App.css";
 
-import './App.css'
 import { SelectRole } from "./page/login/SelectRole";
 import { AdminLogin } from "./page/login/AdminLogin";
 import { UserLogin } from "./page/login/UserLogin";
+import { UserLayout } from "./page/user/Layout";
+import { Dashboard } from "./page/user/Dashboard/Dashboard";
+// import { Layout } from "./page/user/Layout";
 
 function App() {
-  
-
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <SelectRole/>
+    },
+    {
+      path: "adminLogin",
+      element: <AdminLogin/>
+    },
+    {
+      path: "userLogin",
+      element: <UserLogin/>
+    },
+    {
+      path: "user",
+      element: <UserLayout/>,
+      children:[
+        {
+          path: "dashboard",
+          element: <Dashboard/>
+        }
+      ]
+    },
+  ]);
   return (
     <>
       <div className="warper">
-          <Routes>
-            <Route path="/" element={<SelectRole/>} />
-            <Route path="adminLogin" element={<AdminLogin/>} />
-            <Route path="userLogin" element={<UserLogin/>} />
-          </Routes>
+        <RouterProvider router={router}/>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
