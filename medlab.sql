2e1976553ac2240e1c5ab940a4e65737842062c3
AnemoneTK <Copp.teenkung@gmail.com>
2024-04-03 20:50:57 +0700

    [UPDATE]-update backup sql

--- medlab.sql
+++ medlab.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Mar 18, 2024 at 03:56 PM
+-- Generation Time: Apr 03, 2024 at 01:50 PM
 -- Server version: 5.7.39
 -- PHP Version: 7.4.33
 
@@ -43,24 +43,134 @@ INSERT INTO `category` (`category_id`, `category_name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `export`
+--
+
+CREATE TABLE `export` (
+  `export_id` int(5) UNSIGNED ZEROFILL NOT NULL,
+  `lot_id` varchar(20) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location`
+--
+
+CREATE TABLE `location` (
+  `location_id` int(5) UNSIGNED ZEROFILL NOT NULL,
+  `Location_name` varchar(20) NOT NULL,
+  `warehouse_id` int(5) UNSIGNED ZEROFILL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `location`
+--
+
+INSERT INTO `location` (`location_id`, `Location_name`, `warehouse_id`) VALUES
+(00001, 'A1', 00001),
+(00002, 'A2', 00001),
+(00003, 'B1', 00002),
+(00004, 'B2', 00002);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lot`
+--
+
+CREATE TABLE `lot` (
+  `lot_id` int(5) UNSIGNED ZEROFILL NOT NULL,
+  `p_id` varchar(20) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `due_date` date DEFAULT NULL,
+  `exp_date` date DEFAULT NULL,
+  `before_date` int(5) DEFAULT '30',
+  `location_id` int(5) UNSIGNED ZEROFILL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `lot`
+--
+
+INSERT INTO `lot` (`lot_id`, `p_id`, `quantity`, `due_date`, `exp_date`, `before_date`, `location_id`) VALUES
+(00001, '1', 40, '2024-04-01', '2024-04-04', 50, 00001),
+(00002, '2', 150, '2024-04-01', '2024-04-27', 30, 00002),
+(00003, '1', 150, NULL, NULL, 30, NULL),
+(00004, '2', 150, NULL, NULL, 30, NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `product`
 --
 
 CREATE TABLE `product` (
-  `id` int(5) NOT NULL,
+  `id` varchar(20) NOT NULL,
   `name` varchar(20) NOT NULL,
+  `unit` int(5) NOT NULL,
   `type` int(5) NOT NULL,
-  `category` int(5) NOT NULL
+  `category` int(5) NOT NULL,
+  `detail` text,
+  `direction` text,
+  `low_stock` int(5) NOT NULL DEFAULT '50'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`id`, `name`, `type`, `category`) VALUES
-(1, 'พารา', 1, 1),
-(2, 'ยานอนหลับ', 2, 2),
-(3, 'Test', 3, 2);
+INSERT INTO `product` (`id`, `name`, `unit`, `type`, `category`, `detail`, `direction`, `low_stock`) VALUES
+('1', '1', 2, 2, 1, '1', '1', 50),
+('2', '2', 1, 2, 1, '2', '2', 150),
+('3', '3', 3, 3, 2, '3', '3', 50),
+('4', 'Test 4', 1, 2, 2, '4', '4', 50),
+('5', 'Test5', 3, 3, 2, '5', '5', 150);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase`
+--
+
+CREATE TABLE `purchase` (
+  `purchase_id` int(5) UNSIGNED ZEROFILL NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `purcher` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `purchase`
+--
+
+INSERT INTO `purchase` (`purchase_id`, `date`, `purcher`) VALUES
+(00001, '2024-03-30 16:49:47', 'Test Account'),
+(00002, '2024-04-01 17:00:37', 'Test Account'),
+(00003, '2024-04-02 12:25:59', 'Test Account'),
+(00004, '2024-04-02 13:48:37', 'Test Account');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase_detail`
+--
+
+CREATE TABLE `purchase_detail` (
+  `id` int(5) NOT NULL,
+  `purchase_id` int(5) UNSIGNED ZEROFILL NOT NULL,
+  `lot_id` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `purchase_detail`
+--
+
+INSERT INTO `purchase_detail` (`id`, `purchase_id`, `lot_id`) VALUES
+(1, 00001, 1),
+(2, 00001, 2),
+(3, 00004, 3),
+(4, 00004, 4);
 
 -- --------------------------------------------------------
 
@@ -85,15 +195,89 @@ INSERT INTO `type` (`type_id`, `type_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `useraccount`
+-- Table structure for table `unit`
 --
 
-CREATE TABLE `useraccount` (
-  `id` int(11) NOT NULL,
-  `userName` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `name` varchar(20) NOT NULL
+CREATE TABLE `unit` (
+  `unit_id` int(5) NOT NULL,
+  `unit_name` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `unit`
+--
+
+INSERT INTO `unit` (`unit_id`, `unit_name`) VALUES
+(1, 'ลัง'),
+(2, 'กล่อง'),
+(3, 'โหล');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_name` varchar(20) NOT NULL,
+  `user_password` text NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `surname` varchar(20) NOT NULL,
+  `role` int(5) NOT NULL,
+  `withdraw` tinyint(1) NOT NULL DEFAULT '0',
+  `add_new` tinyint(1) NOT NULL DEFAULT '0',
+  `purchase` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_name`, `user_password`, `name`, `surname`, `role`, `withdraw`, `add_new`, `purchase`) VALUES
+('65039089', '$2b$10$MSxxqHS.vfm5y0AxKI0PhODFcDVQElWFvmT82BWXF75wp.O/Xi8Ge', 'Narisara', 'Changsadao', 2, 1, 1, 0),
+('Admin', '$2b$10$2Wy035BANg1REJtcnVFx4ehPMXGrJaC0Yi0a.uWSOGevJ2nZUzqQi', 'Admin', 'Account', 1, 1, 1, 0),
+('EMP', '$2b$10$li.x1ZTBx7jHA83je.los.ksZT6SGLFRzZKWmCWKtq.E8nxk2nQzW', 'User2', 'Account', 2, 0, 0, 0),
+('Manager', '$2b$10$TsR0R1mhtpAJy9FmsAAUMOBqxdsrrmUqCbheWa3gNch0F.0WMBRHy', 'User', 'Account', 2, 1, 1, 1),
+('Test', '$2b$10$UgjvKYhxLnGPss2z4hxQf.FghuvFqK2NdhJFvVtu3mkzIAoXEbvMa', 'Test', 'Account', 2, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
+
+CREATE TABLE `user_role` (
+  `role_id` int(5) NOT NULL,
+  `role_name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`role_id`, `role_name`) VALUES
+(1, 'admin'),
+(2, 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `warehouse`
+--
+
+CREATE TABLE `warehouse` (
+  `warehouse_id` int(5) UNSIGNED ZEROFILL NOT NULL,
+  `warehouse_name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `warehouse`
+--
+
+INSERT INTO `warehouse` (`warehouse_id`, `warehouse_name`) VALUES
+(00001, 'A'),
+(00002, 'B'),
+(00003, 'C');
 
 --
 -- Indexes for dumped tables
@@ -106,12 +290,43 @@ ALTER TABLE `category`
   ADD PRIMARY KEY (`category_id`);
 
 --
+-- Indexes for table `export`
+--
+ALTER TABLE `export`
+  ADD PRIMARY KEY (`export_id`);
+
+--
+-- Indexes for table `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`location_id`);
+
+--
+-- Indexes for table `lot`
+--
+ALTER TABLE `lot`
+  ADD PRIMARY KEY (`lot_id`);
+
+--
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_type` (`type`),
-  ADD KEY `fk_category` (`category`);
+  ADD KEY `fk_category` (`category`),
+  ADD KEY `fk_unit` (`unit`);
+
+--
+-- Indexes for table `purchase`
+--
+ALTER TABLE `purchase`
+  ADD PRIMARY KEY (`purchase_id`);
+
+--
+-- Indexes for table `purchase_detail`
+--
+ALTER TABLE `purchase_detail`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `type`
@@ -120,10 +335,29 @@ ALTER TABLE `type`
   ADD PRIMARY KEY (`type_id`);
 
 --
--- Indexes for table `useraccount`
+-- Indexes for table `unit`
 --
-ALTER TABLE `useraccount`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `unit`
+  ADD PRIMARY KEY (`unit_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_name`),
+  ADD KEY `fk_role` (`role`);
+
+--
+-- Indexes for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- Indexes for table `warehouse`
+--
+ALTER TABLE `warehouse`
+  ADD PRIMARY KEY (`warehouse_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -136,16 +370,58 @@ ALTER TABLE `category`
   MODIFY `category_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `export`
+--
+ALTER TABLE `export`
+  MODIFY `export_id` int(5) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `location`
+--
+ALTER TABLE `location`
+  MODIFY `location_id` int(5) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `lot`
+--
+ALTER TABLE `lot`
+  MODIFY `lot_id` int(5) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `purchase`
+--
+ALTER TABLE `purchase`
+  MODIFY `purchase_id` int(5) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `purchase_detail`
+--
+ALTER TABLE `purchase_detail`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `type`
 --
 ALTER TABLE `type`
   MODIFY `type_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `useraccount`
+-- AUTO_INCREMENT for table `unit`
 --
-ALTER TABLE `useraccount`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `unit`
+  MODIFY `unit_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `role_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `warehouse`
+--
+ALTER TABLE `warehouse`
+  MODIFY `warehouse_id` int(5) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -156,7 +432,14 @@ ALTER TABLE `useraccount`
 --
 ALTER TABLE `product`
   ADD CONSTRAINT `fk_category` FOREIGN KEY (`category`) REFERENCES `category` (`category_id`),
-  ADD CONSTRAINT `fk_type` FOREIGN KEY (`type`) REFERENCES `type` (`type_id`);
+  ADD CONSTRAINT `fk_type` FOREIGN KEY (`type`) REFERENCES `type` (`type_id`),
+  ADD CONSTRAINT `fk_unit` FOREIGN KEY (`unit`) REFERENCES `unit` (`unit_id`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `fk_role` FOREIGN KEY (`role`) REFERENCES `user_role` (`role_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
